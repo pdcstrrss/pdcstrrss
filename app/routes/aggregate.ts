1b3f1ab02f2683b93b35231fa660df4fda23d366
Maarten Van Hoof <info@mrtnvh.com>
2022-04-19 08:54:35 +0200

    fix(aggregator): remove feed from response data

--- app/routes/aggregate.ts
+++ app/routes/aggregate.ts
@@ -1,11 +1,10 @@
-import { LoaderFunction } from "remix";
+import { LoaderFunction } from "@remix-run/server-runtime";
 import isNumeric from "validator/lib/isNumeric";
 import aggregator from "~/lib/aggregator";
 import type { EpisodesData, Feed } from "~/lib/types";
 
 export interface AggregateResponse {
   episodes: EpisodesData;
-  feeds: Feed[];
 }
 export const loader: LoaderFunction = async ({ request }): Promise<AggregateResponse> => {
   const url = new URL(request.url);
@@ -22,6 +21,6 @@ export const loader: LoaderFunction = async ({ request }): Promise<AggregateResp
     offset = isNumeric(offsetParam) ? Number(offsetParam) : undefined;
   }
 
-  const { episodes, feeds } = await aggregator({ limit, offset });
-  return { episodes, feeds };
+  const { episodes } = await aggregator({ limit, offset });
+  return { episodes };
 };
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -115,8 +115,6 @@ function AuthenticatedIndex({ loaderData, user }: { loaderData: AggregateRespons
 export default function Index() {
   const { data, user } = useLoaderData<{ data: AggregateResponse; user: User }>();
 
-  console.log(data, user);
-
   return (
     <div data-page-index>{!!user ? <AuthenticatedIndex loaderData={data} user={user} /> : <AnonymousIndex />}</div>
   );
