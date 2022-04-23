79bd83ed1aa95c61dee173e0dc5a6316b2d99a19
Maarten Van Hoof <info@mrtnvh.com>
2022-04-23 11:12:59 +0200

    refactor(index): improve root page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -14,24 +14,26 @@ export default function Index() {
   return (
     <div data-page-index>
       <div data-anonymous-index>
-        <h1>PDCSTRRSS</h1>
-        <p>The RSS PodCast PWA</p>
-        <ul className="list-unstyled">
-          <li>Login with GitHub</li>
-          <li>
-            Sponsor the project for only <strong>$1 / month</strong>
-          </li>
-          <li>Configure</li>
-          <li>Listen</li>
-        </ul>
-        <form action="/auth/github" method="post" style={{ display: "inline-block" }}>
-          <Button>
-            <svg style={{ inlineSize: "var(--space)", blockSize: "var(--space)" }}>
-              <use xlinkHref="#github" />
-            </svg>
-            <span>Login with GitHub</span>
-          </Button>
-        </form>
+        <div data-card>
+          <h1 data-title>PDCSTRRSS</h1>
+          <p><strong>The RSS PodCast PWA</strong></p>
+          <ul className="list-unstyled">
+            <li>Login with GitHub</li>
+            <li>
+              Sponsor the project for only <strong>$1 / month</strong>
+            </li>
+            <li>Configure</li>
+            <li>Listen</li>
+          </ul>
+          <form action="/auth/github" method="post" style={{ display: "inline-block" }}>
+            <Button>
+              <svg style={{ inlineSize: "var(--space)", blockSize: "var(--space)" }}>
+                <use xlinkHref="#github" />
+              </svg>
+              <span>Login with GitHub</span>
+            </Button>
+          </form>
+        </div>
       </div>
     </div>
   );
