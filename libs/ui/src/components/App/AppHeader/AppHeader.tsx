0ed6d700bf02cdf16fc7780f7f58d1996b62897d
Maarten Van Hoof <info@mrtnvh.com>
2022-04-27 17:24:21 +0200

    feat(AppHeader): add navigation

--- libs/ui/src/components/App/AppHeader/AppHeader.tsx
+++ libs/ui/src/components/App/AppHeader/AppHeader.tsx
@@ -1,9 +1,9 @@
-import { User } from "@pdcstrrss/database";
-import styles from "./AppHeader.css";
+import { User } from '@pdcstrrss/database';
+import styles from './AppHeader.css';
 
-export const AppHeaderLinks = () => [{ rel: "stylesheet", href: styles }];
+export const AppHeaderLinks = () => [{ rel: 'stylesheet', href: styles }];
 
-export type AppHeaderUser = Pick<User, "displayName" | "image">;
+export type AppHeaderUser = Pick<User, 'displayName' | 'image'>;
 
 interface AppHeaderProps {
   user: AppHeaderUser;
@@ -13,7 +13,11 @@ export function AppHeader({ user }: AppHeaderProps) {
   return (
     <header data-app-header>
       <h1 data-app-header-title>PODCSTRRSS</h1>
-      {user.image && <img  data-app-header-image src={user.image} alt={user.displayName} />}
+      <nav>
+        <a href="/episodes">Episodes</a>
+        <a href="/feeds">Feeds</a>
+      </nav>
+      {user.image && <img data-app-header-image src={user.image} alt={user.displayName} />}
     </header>
   );
 }
