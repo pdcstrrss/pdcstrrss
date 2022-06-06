647f33643bcea9f6e85b8b9cf1dd0cff0b607d0b
Maarten Van Hoof <info@mrtnvh.com>
2022-06-06 23:05:52 +0200

    feat: add Sentry

---
+++ apps/web-remix/app/routes/__anonymous/privacy.tsx
@@ -0,0 +1,12 @@
+export default function Privacy() {
+  return (
+    <article className="page container container-md">
+      <header className="page-header">
+        <h1 className="page-header-title">Privacy</h1>
+      </header>
+      <section>
+
+      </section>
+    </article>
+  );
+}
