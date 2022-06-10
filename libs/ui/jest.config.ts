656cf5a36f9ce794cb53a3a56058039dfb7e1ba4
Maarten Van Hoof <info@mrtnvh.com>
2022-06-10 23:06:14 +0200

    chore: update nx and dependencies

--- libs/ui/jest.config.ts
+++ libs/ui/jest.config.ts
@@ -1,6 +1,7 @@
-module.exports = {
+/* eslint-disable */
+export default {
   displayName: 'ui',
-  preset: '../../jest.preset.ts',
+  preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
