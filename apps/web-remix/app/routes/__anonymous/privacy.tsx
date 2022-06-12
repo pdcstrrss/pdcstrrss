ddb41d236e207e3a49c5e223540a02641b9dbb5b
Maarten Van Hoof <info@mrtnvh.com>
2022-06-12 09:33:57 +0200

    wip - privacy page

--- apps/web-remix/app/routes/__anonymous/privacy.tsx
+++ apps/web-remix/app/routes/__anonymous/privacy.tsx
@@ -6,6 +6,22 @@ export default function Privacy() {
       </header>
       <section>
 
+        <p>So, what external services do we use and why?</p>
+
+        <h2 className="h4">Netlify</h2>
+        <h2 className="h4">Planetscale</h2>
+
+        <h2 className="h4">Sentry</h2>
+        <p>
+          Writing good software is hard. Making mistakes is human. Things will break and when they do, we want to know
+          why, <strong>without</strong> you having to tell us. Something breaks? Sentry (hopefully) will shout at us and
+          give us insight in to the cause of why things are breaking.
+
+          More about how Sentry processes it's data can be found in <a href="https://sentry.io/legal/dpa/" rel="noopener noreferrer">their DPA</a>.
+        </p>
+
+        <h2 className="h4">Plausible</h2>
+        <h2 className="h4"></h2>
       </section>
     </article>
   );
