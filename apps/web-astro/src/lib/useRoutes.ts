c28b3a7a83eb56a8e2d07636195191b60888d807
Maarten Van Hoof <maarten.vanhoof@iodigital.com>
2022-12-21 21:10:24 +0100

    wip - astro - Feeds list

--- apps/web-astro/src/lib/useRoutes.ts
+++ apps/web-astro/src/lib/useRoutes.ts
@@ -1,4 +1,4 @@
-export default {
+export const ROUTES = {
   app: '/app',
   privacy: '/privacy',
   get feeds() {
--- apps/web-astro/src/lib/useUser.ts
+++ apps/web-astro/src/lib/useUser.ts
@@ -1,6 +1,11 @@
 import type { User, Episode } from '@prisma/client';
 import { getUser } from '@astro-auth/core';
-import { getUserByGitHubId, getEpisodeWithStatusPlaying } from '@pdcstrrss/core';
+import {
+  getUserByGitHubId,
+  getEpisodeWithStatusPlaying,
+  getUserSponsorship,
+  exceedsFreeFeedThreshold,
+} from '@pdcstrrss/core';
 import type { UserSession } from './useUserTypes';
 import type { AstroGlobal } from 'astro';
 
@@ -9,16 +14,33 @@ export const getUserSessionFromRequest = ({ request }: { request: Request }): Us
 
 export type UserFromRequest = User & {
   activeEpisode: Episode | null;
+  permissions: {
+    canCreateFeed: boolean;
+    canCreateFreeFeeds: boolean;
+  };
 };
+
+export async function getUserPermissions({ user, userSession }: { user: User; userSession: UserSession }) {
+  const sponsorship = await getUserSponsorship({ accessToken: userSession.accessToken });
+  const canCreateFreeFeeds = await exceedsFreeFeedThreshold({ userId: user?.id });
+  const canCreateFeed = canCreateFreeFeeds || sponsorship.sponsor || sponsorship.member;
+  return {
+    canCreateFeed,
+    canCreateFreeFeeds,
+  };
+}
 
 export async function getUserFromRequest({ request }: Readonly<AstroGlobal<Record<string, any>>>) {
   const userSession = getUserSessionFromRequest({ request });
   if (!userSession) return;
   const user = await getUserByGitHubId(userSession.user.id);
   if (!user) return;
-  const activeUserEpisode = await getEpisodeWithStatusPlaying(user?.id);
+  const activeEpisode = await getEpisodeWithStatusPlaying(user?.id);
+  const permissions = await getUserPermissions({ user, userSession });
+
   return {
     ...user,
-    activeEpisode: activeUserEpisode,
+    activeEpisode,
+    permissions,
   } as UserFromRequest;
 }
