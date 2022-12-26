bd21e49332b5127734e5a336342a0ffbe7283a81
Maarten Van Hoof <maarten.vanhoof@iodigital.com>
2022-12-26 08:08:33 +0100

    chore: remove Remix

--- apps/web-remix/app/routes/__anonymous/auth/github/index.tsx
+++
@@ -1,4 +0,0 @@
-import { LoaderFunction } from '@remix-run/server-runtime';
-import { authenticator } from '../../../../services/auth.server';
-
-export const loader: LoaderFunction = ({ request }) => authenticator.authenticate('github', request);
