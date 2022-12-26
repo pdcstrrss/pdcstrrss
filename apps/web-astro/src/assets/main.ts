3e034a633bdb16a10f4ea8c3c4752d63dee2a04a
Maarten Van Hoof <maarten.vanhoof@iodigital.com>
2022-12-26 07:31:54 +0100

    feat(form): progressive form submit

--- apps/web-astro/src/assets/main.ts
+++ apps/web-astro/src/assets/main.ts
@@ -1,6 +1,19 @@
 import './polyfills';
 
+import { handleFormSubmit } from './form';
+
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import Swup from 'swup';
-const swup = new Swup(); // only this line when included with script tag
+
+//
+// Page transitions
+//
+const swup = new Swup();
+
+//
+// Form submissions
+//
+document.body.addEventListener('submit', (event) => {
+  if (event.target instanceof HTMLFormElement) return handleFormSubmit({ event, swup });
+});
