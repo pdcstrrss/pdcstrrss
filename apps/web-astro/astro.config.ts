94f7720e996927bf0bb7cd422df73b50a03f617b
Maarten Van Hoof <maarten.vanhoof@iodigital.com>
2022-12-28 07:30:21 +0100

    chore: remove turbolinks dependency

--- apps/web-astro/astro.config.ts
+++ apps/web-astro/astro.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import sitemap from '@astrojs/sitemap';
-import turbolinks from '@astrojs/turbolinks';
 import react from '@astrojs/react';
 
 // import { FontaineTransform } from 'fontaine';
 
 export default defineConfig({
   outDir: '../../dist/apps/web-astro',
-  integrations: [react(), sitemap(), turbolinks()],
+  integrations: [react(), sitemap()],
   output: 'server',
   adapter: netlify(),
   // vite: {
