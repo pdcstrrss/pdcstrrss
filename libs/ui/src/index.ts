0a623a274940f8707ddc5ec41803e22162a8ceae
Maarten Van Hoof <maarten.vanhoof@iodigital.com>
2023-06-18 09:44:02 +0200

    test(ui): refactor to vitest

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,3 +1,2 @@
 export * from './components/index.js';
 export * from './constants/index.js';
-export * from './views/index.js';
--- libs/ui/src/views/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './AuthenticatedLayout/index.js';
-export * from './IndexView/index.js';
